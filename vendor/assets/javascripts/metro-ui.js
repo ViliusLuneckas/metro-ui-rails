9347e584ce7f19ee88de46d7a847ca7dd4ac6f08
Vilius Luneckas <vilius.luneckas@gmail.com>
2012-11-11 19:29:09 +0200

    New javascripts, stylesheets.

--- vendor/assets/javascripts/metro-ui.js
+++ vendor/assets/javascripts/metro-ui.js
@@ -1,4 +1,8 @@
 //= require metro-ui/accordion
 //= require metro-ui/buttonset
+//= require metro-ui/carousel
 //= require metro-ui/dropdown
 //= require metro-ui/pagecontrol
+//= require metro-ui/rating
+//= require metro-ui/slider
+//= require metro-ui/tile-slider
