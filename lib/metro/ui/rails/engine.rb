51e7454b213bddb063c722572a3b9698994333f0
Vilius Luneckas <vilius.luneckas@gmail.com>
2013-01-07 23:19:14 +0200

    Fixing assets path.

--- lib/metro/ui/rails/engine.rb
+++ lib/metro/ui/rails/engine.rb
@@ -8,7 +8,7 @@ module Metro
           :after => 'less-rails.after.load_config_initializers', 
           :group => :all do |app|
             app.config.less.paths << File.join(config.root, 'vendor', 'toolkit')
-            app.config.less.paths << File.join(config.root, 'vendor', 'fonts')
+            app.config.assets.paths << File.join(config.root, 'vendor', 'fonts')
         end
       end
     end
