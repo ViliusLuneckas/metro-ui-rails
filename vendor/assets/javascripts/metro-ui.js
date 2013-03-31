45a27ddc84f4e1e6d71da9abe9ef760e443d887f
Vilius Luneckas <vilius.luneckas@gmail.com>
2013-03-31 13:06:15 +0300

    Updating less files.

---
+++ vendor/assets/javascripts/metro-ui.js
@@ -0,0 +1,13 @@
+//= require metro-ui/pagecontrol
+//= require metro-ui/accordion
+//= require metro-ui/calendar
+//= require metro-ui/tile-drag
+//= require metro-ui/dropdown
+//= require metro-ui/start-menu
+//= require metro-ui/dialog
+//= require metro-ui/tile-slider
+//= require metro-ui/carousel
+//= require metro-ui/buttonset
+//= require metro-ui/input-control
+//= require metro-ui/rating
+//= require metro-ui/slider
